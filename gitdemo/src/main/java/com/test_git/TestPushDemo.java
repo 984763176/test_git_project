38838310c54fcdcb15d8fec24f5872f8a788698c
peter <peter@atguigu.com>
2020-09-01 19:22:51 +0800

    新增了TestPushDemo.java文件

---
+++ gitdemo/src/main/java/com/test_git/TestPushDemo.java
@@ -0,0 +1,12 @@
+package com.test_git;
+
+/**
+ * @description: TODO
+ * @author: HaoWu
+ * @create: 2020年09月01日
+ */
+public class TestPushDemo {
+    public static void main(String[] args) {
+
+    }
+}
