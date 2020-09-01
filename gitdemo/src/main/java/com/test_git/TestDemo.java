ca01dda6b00fe426bed698fe83f8175da66a5de2
peter <peter@atguigu.com>
2020-09-01 18:46:51 +0800

    hotfix分支 修改了文件

--- gitdemo/src/main/java/com/test_git/TestDemo.java
+++ gitdemo/src/main/java/com/test_git/TestDemo.java
@@ -9,7 +9,7 @@ public class TestDemo {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hello world");
-        System.out.println("hello world");
+        System.out.println("hello world");// edit by hotfix
         System.out.println("hello world");
         System.out.println("hello world");
     }
