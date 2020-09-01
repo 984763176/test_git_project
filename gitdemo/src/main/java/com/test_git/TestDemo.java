fe899284021b67aeb2b2dff962c58fa69d3aecac
peter <peter@atguigu.com>
2020-09-01 18:32:04 +0800

    idea的project创建的本地库，提交文件

---
+++ gitdemo/src/main/java/com/test_git/TestDemo.java
@@ -0,0 +1,16 @@
+package com.test_git;
+
+/**
+ * @description: 测试idea集成git
+ * @author: HaoWu
+ * @create: 2020年09月01日
+ */
+public class TestDemo {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+    }
+}
